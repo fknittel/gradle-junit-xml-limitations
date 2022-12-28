bb4e4749269a462cf79e5b3f88701a7d02d9f510
Fabian Knittel <fabian.knittel@lettink.de>
2022-12-28 12:37:35 +0100

    improve naming

--- src/test/java/org/example/gradle/junit/xml/limitations/FailingParameterizedTest.java
+++ src/test/java/org/example/gradle/junit/xml/limitations/FailingParameterizedTest.java
@@ -3,25 +3,22 @@ package org.example.gradle.junit.xml.limitations;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.stream.Stream;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-class JUnitXmlLimitationsTest {
+class FailingParameterizedTest {
     static Stream<String> parameters() {
         return Stream.of("firstParameter", "secondParameter");
     }
 
     @ParameterizedTest
     @MethodSource("parameters")
-    @DisplayName("first method")
     void firstMethod(String parameter) {
         fail("first method failing with parameter " + parameter);
     }
 
     @ParameterizedTest
     @MethodSource("parameters")
-    @DisplayName("second method")
     void secondMethod(String parameter) {
         fail("second method failing with parameter " + parameter);
     }
